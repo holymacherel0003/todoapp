1e755fe2a6391f2b4bf9fe8a77bc9d7eac282139
holymacherel0003 <sh.nakajima0003@livela.jp>
2019-01-04 15:13:51 +0900

    #9 Completeリストの分割

--- src/renderer/components/TodoBase.vue
+++ src/renderer/components/TodoBase.vue
@@ -4,38 +4,61 @@
       <todo-editor v-bind="editorProps" v-on:edit="completeEdit"></todo-editor>
     </b-modal>
 		<b-field>
-			<b-input v-model="text" minlength="1" maxlength="30" placeholder="Add Task"></b-input>
+			<b-input v-model="text" minlength="1" maxlength="30" placeholder="Input Task Title"></b-input>
 			<p class="control">
 				<button	class="button is-primary" v-on:click="addTodo()">＋</button>
 			</p>
 		</b-field>
 		<section>
-			<b-collapse class="card" v-for="task in tasks" v-bind:data="task" v-bind:key="task.id" v-bind:class="{'complete': task.complete}">
-				<div slot="trigger" slot-scope="props" class="card-header">
-          <p class="card-header-title">
-              {{ task.title }}
-          </p>
-          <a class="card-header-icon">
-            <b-icon
-                :icon="props.open ? 'menu-down' : 'menu-up'">
-            </b-icon>
-          </a>
-        </div>
-        <div class="card-content">
-          <div class="content">
-            {{ task.detail }}
-          </div>
-        </div>
-        <footer class="card-footer">
-          <a class="card-footer-item" 
-            v-model="task.complete" 
-            v-on:click="toggleComplete(task)">
-              {{ task.complete ? 'Uncomplete' : 'Complete' }}
-            </a>
-          <a class="card-footer-item" v-on:click="editTodo(task)">Edit</a>
-          <a class="card-footer-item" v-on:click="deleteTodo(task)">Delete</a>
-        </footer>
-			</b-collapse>
+      <b-tabs size="is-medium"  position="is-centered" expanded v-model="activeTab">
+        <b-tab-item label="Todos" icon="list">
+          <b-collapse class="card" v-for="task in tasks" v-bind:data="task" v-bind:key="task.id">
+            <div slot="trigger" slot-scope="props" class="card-header">
+              <p class="card-header-title" >
+                {{ task.title }}
+              </p>
+              <a class="card-header-icon">
+                <b-icon
+                    :icon="props.open ? 'angle-up' : 'angle-down'">
+                </b-icon>
+              </a>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                {{ task.detail }}
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a class="card-footer-item" v-on:click="completeTodo(task)">Complete</a>
+              <a class="card-footer-item" v-on:click="editTodo(task)">Edit</a>
+            </footer>
+          </b-collapse>
+        </b-tab-item>
+          
+        <b-tab-item label="Completed" icon="check">
+          <b-collapse class="card" v-for="task in completeTasks" v-bind:data="task" v-bind:key="task.id">
+            <div slot="trigger" slot-scope="props" class="card-header">
+              <p class="card-header-title">
+                  {{ task.title }}
+              </p>
+              <a class="card-header-icon">
+                <b-icon
+                    :icon="props.open ? 'angle-up' : 'angle-down'">
+                </b-icon>
+              </a>
+            </div>
+            <div class="card-content">
+              <div class="content">
+                {{ task.detail }}
+              </div>
+            </div>
+            <footer class="card-footer">
+              <a class="card-footer-item" v-on:click="uncompleteTodo(task)">Uncomplete</a>
+              <a class="card-footer-item" v-on:click="deleteTodo(task)">Delete</a>
+            </footer>
+          </b-collapse>
+        </b-tab-item>
+      </b-tabs>
 		</section>
   </div>
 </template>
@@ -51,17 +74,12 @@ export default {
 
   data () {
     return {
+      activeTab: 0,
       text: '',
       id: 1,
       isComponentModalActive: false,
-      tasks: [
-        {
-          id: 0,
-          title: 'タスク1',
-          detail: 'hogehogehoge',
-          complete: false
-        }
-      ],
+      tasks: [],
+      completeTasks: [],
       editorProps: {
         id: 0,
         title: '',
@@ -73,6 +91,7 @@ export default {
   mounted () {
     this.id = localStorage.getItem('id')
     this.tasks = JSON.parse(localStorage.getItem('tasks')) || []
+    this.completeTasks = JSON.parse(localStorage.getItem('completeTasks')) || []
   },
 
   methods: {
@@ -80,8 +99,7 @@ export default {
       const addData = {
         id: this.id,
         title: this.text,
-        detail: '',
-        complete: false
+        detail: ''
       }
       this.tasks.push(addData)
       this.id++
@@ -89,12 +107,20 @@ export default {
       this.setItems()
     },
 
-    toggleComplete (context) {
-      if (context.complete) {
-        context.complete = false
-      } else {
-        context.complete = true
-      }
+    completeTodo (context) {
+      this.completeTasks.push(context)
+      this.tasks = this.tasks.filter((element) => {
+        return element.id !== context.id
+      })
+      this.setItems()
+    },
+
+    uncompleteTodo (context) {
+      this.tasks.push(context)
+      this.completeTasks = this.completeTasks.filter((element) => {
+        return element.id !== context.id
+      })
+      this.setItems()
     },
 
     editTodo (context) {
@@ -115,12 +141,16 @@ export default {
       this.tasks = this.tasks.filter((element) => {
         return element.id !== context.id
       })
+      this.completeTasks = this.completeTasks.filter((element) => {
+        return element.id !== context.id
+      })
       this.setItems()
     },
 
     setItems () {
       localStorage.setItem('id', this.id)
       localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      localStorage.setItem('completeTasks', JSON.stringify(this.completeTasks))
     }
   }
 }
@@ -128,13 +158,8 @@ export default {
 
 <style scoped lang="scss">
 .todo {
-  width: 500px;
+  width: 600px;
   margin: auto;
   padding: 20px 5px;
-
-  .complete {
-    background: #ccc;
-    color: #666;
-  }
 }
 </style>
